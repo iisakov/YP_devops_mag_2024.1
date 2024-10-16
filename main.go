0c2090a11237c9f6161fc833ac89f6f63760eec0
iisakov <isakov.arh@yandex.ru>
2024-10-16 21:52:00 +0300

    init

--- main.go
+++ main.go
@@ -1,16 +1,3 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-)
-
-func main() {
-	r, err := http.Get("srv.msk01.gigacorp.local/_stats")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(r)
-}
+func main() {}
